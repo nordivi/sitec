d904da241da22d50b0d0e06707e91db4b2245d61
nordivi <victorw.nordi@icloud.com>
2024-05-18 17:10:29 -0300

    small changes

--- functions.js
+++ functions.js
@@ -10,29 +10,3 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
-
-const chBoxes = 
-            document.querySelectorAll('.dropdown-menu input[type="checkbox"]'); 
-        const dpBtn =  
-            document.getElementById('multiSelectDropdown'); 
-        let mySelectedListItems = []; 
-  
-        function handleCB() { 
-            mySelectedListItems = []; 
-            let mySelectedListItemsText = ''; 
-  
-            chBoxes.forEach((checkbox) => { 
-                if (checkbox.checked) { 
-                    mySelectedListItems.push(checkbox.value); 
-                    mySelectedListItemsText += checkbox.value + ', '; 
-                } 
-            }); 
-  
-            dpBtn.innerText = 
-                mySelectedListItems.length > 0 
-                    ? mySelectedListItemsText.slice(0, -2) : 'Select'; 
-        } 
-  
-        chBoxes.forEach((checkbox) => { 
-            checkbox.addEventListener('change', handleCB); 
-        }); 
