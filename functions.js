b5895bb4ceed8bef192f06a3728cb9ce60a46412
nordivi <victorw.nordi@icloud.com>
2024-05-22 20:28:39 -0300

    :sparkles: multiselect

--- functions.js
+++ functions.js
@@ -30,3 +30,16 @@ setInterval(() => {
     i=0;
   }
 },2000)
+
+var expanded = false;
+
+function showCheckboxes() {
+  var checkboxes = document.getElementById("checkboxes");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
