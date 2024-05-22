0924d54f27f3c96d8f000e01b8c65472cabcea0a
nordivi <victorw.nordi@icloud.com>
2024-05-22 19:55:25 -0300

    :sparkles: carrousel e formulario de contato

--- functions.js
+++ functions.js
@@ -10,3 +10,23 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+const carouselItems = document.querySelectorAll(".carousel_item"); 
+let i = 1;
+
+setInterval(() => {
+ Array.from(carouselItems).forEach((item,index) => {
+
+   if(i < carouselItems.length){
+    item.style.transform = `translateX(-${i*100}%)`
+   }
+  })
+
+
+  if(i < carouselItems.length){
+    i++;
+  }
+  else{
+    i=0;
+  }
+},2000)
